98da8a519f84e387948c5ac71c4463550c6f0280
Stas Kobles <sskobles@gmail.com>
2022-11-28 04:52:51 +0400

    Add MongoDB

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,15 @@
+import { TrackModule } from './track/track.module';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({ controllers: [], providers: [] })
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TrackModule,
+    MongooseModule.forRoot(
+      `mongodb+srv://alca:${process.env.DATABASE_PASSWORD}@cluster0.7zsz9qf.mongodb.net/?retryWrites=true&w=majority`,
+    ),
+  ],
+})
 export class AppModule {}
