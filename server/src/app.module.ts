0e3ff6cb568bff6facc568955fab9c8e23bc2d80
Stas Kobles <sskobles@gmail.com>
2022-11-29 18:51:06 +0400

    Server part on NestJS

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,19 @@
-import { TrackModule } from './track/track.module';
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { FileModule } from './file/file.module';
+import { TrackModule } from './track/track.module';
+import * as path from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static'),
+    }),
     ConfigModule.forRoot(),
     TrackModule,
+    FileModule,
     MongooseModule.forRoot(
       `mongodb+srv://alca:${process.env.DATABASE_PASSWORD}@cluster0.7zsz9qf.mongodb.net/?retryWrites=true&w=majority`,
     ),
--- server/src/track/schemas/track.schema.ts
+++ server/src/track/schemas/track.schema.ts
@@ -13,7 +13,7 @@ export class Track {
   listens: number;
 
   @Prop()
-  track: string;
+  text: string;
 
   @Prop()
   artist: string;
